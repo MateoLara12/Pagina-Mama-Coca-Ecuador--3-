8ab5754c2d4205dec67352b4ab192e8ebe630ce6
MateoLara12 <mateo.lara@uisek.edu.ec>
2026-09-18 10:59:26 -0500

    Entrega concurso

---
+++ Pagina Mama Coca Ecuador/js/main.js
@@ -0,0 +1,18 @@
+//Script para el estilo de las fotos
+//Función 1
+
+const photoContainer = document.getElementById("photo-container");
+
+let content ="";
+for(let i=0; i<20; i++){
+        content += getPhotoElement(i+1);
+        
+}
+
+photoContainer.innerHTML = content;
+
+function getPhotoElement(photo_id){
+    return `<a href="#" class="photo">
+    <img alt="Photo ${photo_id}" src="img/photo_${photo_id}.jpg"/>
+    </a>`;
+}
